ebbf5081173336e8382fd83a858d3f593333b1fe
Sujatabatra <sujatabatra@hotmail.com>
2022-05-10 10:07:53 +0530

    Add files via upload

---
+++ Day30/MyPerformanceSpringAnnotationConfigProjectPart3/src/com/sujata/producer/Singer.java
@@ -0,0 +1,24 @@
+package com.sujata.producer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("Harshit")
+public class Singer implements Performer {
+
+	private String song;
+	
+	
+	public Singer(@Value("Khamoshiya") String song) {
+		super();
+		this.song = song;
+	}
+
+
+	@Override
+	public void perform() {
+		System.out.println("Singer in singing "+song+"!!!!");
+
+	}
+
+}
