9276a163362746c42f9285c526b8d62ca73b4163
Sujatabatra <sujatabatra@hotmail.com>
2022-05-12 09:50:44 +0530

    Add files via upload

---
+++ Day32/SpringJdbcEmployeeLayeredProject/src/com/sujata/client/EmployeeClient.java
@@ -0,0 +1,28 @@
+package com.sujata.client;
+
+import java.util.Scanner;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sujata.entity.Employee;
+import com.sujata.presentation.EmployeePresentation;
+import com.sujata.presentation.EmployeePresentationImpl;
+
+public class EmployeeClient {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		ApplicationContext springContainer=new ClassPathXmlApplicationContext("employeejdbc.xml");
+		EmployeePresentation employeePresentation=(EmployeePresentation)springContainer.getBean("empPresentation");
+
+		while (true) {
+			employeePresentation.showMenu();
+			System.out.println("Enter Choice : ");
+			int choice=scanner.nextInt();
+			employeePresentation.performChoice(choice);
+		}
+
+	}
+
+}
