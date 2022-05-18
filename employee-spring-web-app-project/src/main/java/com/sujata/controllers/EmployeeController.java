81e655b6379714c91610c237eefec319583ee536
Sujatabatra <sujatabatra@hotmail.com>
2022-05-18 09:44:23 +0530

    Add files via upload

---
+++ employee-spring-web-app-project/src/main/java/com/sujata/controllers/EmployeeController.java
@@ -0,0 +1,100 @@
+package com.sujata.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sujata.entity.Employee;
+import com.sujata.service.EmployeeService;
+
+@Controller
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService employeeService;
+	
+//	@RequestMapping("/")
+//	public ModelAndView welcomePageController() {
+//		ModelAndView modelAndView=new ModelAndView();
+//		modelAndView.setViewName("index");
+//		return modelAndView;
+//	}
+	
+	@RequestMapping("/")
+	public String welcomePageController() {
+		return "index";
+	}
+	
+	@RequestMapping("/search")
+	public String searchEmployeePageController() {
+		return "searchEmp";
+	}
+	
+	@RequestMapping("/searchEmp")
+	public ModelAndView searchEmployeeController(@RequestParam("empId") int id) {
+		ModelAndView modelAndView=new ModelAndView();
+//		String id=request.getParameter("empId");
+		Optional<Employee> optionalEmployee=employeeService.searchEmployeeById(id);
+		if(optionalEmployee.isPresent()) {
+			modelAndView.addObject("employee", optionalEmployee.get());
+			modelAndView.setViewName("showEmployee");
+		}
+		else {
+			modelAndView.addObject("message","Employee with ID "+id+"does not exist!");
+			modelAndView.setViewName("output");
+		}
+			return modelAndView;
+	}
+	
+//	@RequestMapping("/searchEmp")
+//	public ModelAndView searchEmployeeController(HttpServletRequest request) {
+//		ModelAndView modelAndView=new ModelAndView();
+//		String id=request.getParameter("empId");
+//		Optional<Employee> optionalEmployee=employeeService.searchEmployeeById(Integer.parseInt(id));
+//		if(optionalEmployee.isPresent()) {
+//			modelAndView.addObject("employee", optionalEmployee.get());
+//			modelAndView.setViewName("showEmployee");
+//		}
+//		else {
+//			modelAndView.addObject("message","Employee with ID "+id+"does not exist!");
+//			modelAndView.setViewName("output");
+//		}
+//			return modelAndView;
+//	}
+	
+	@RequestMapping("/delete")
+	public String deleteEmployeePage() {
+		return "deleteEmp";
+	}
+	
+	@RequestMapping("/deleteEmp")
+	public ModelAndView deleteEmployee(@RequestParam("empId") int eId) {
+		ModelAndView modelAndView=new ModelAndView();
+		String message=null;
+		if(employeeService.deleteEmployeeById(eId))
+			message="Employee with ID "+eId+" deleted !";
+		else
+			message="Employee with ID "+eId+" does not exist !";
+		
+		modelAndView.addObject("message", message);
+		modelAndView.setViewName("output");
+		return modelAndView;
+	}
+	
+	@RequestMapping("/allEmps")
+	public ModelAndView getAllEmployeesController() {
+		ModelAndView modelAndView=new ModelAndView();
+		
+		List<Employee> employees=employeeService.getAllEmployees();
+		modelAndView.addObject("employees", employees);
+		modelAndView.setViewName("showAllEmployees");
+		return modelAndView;
+	}
+}
---
+++ employee-spring-web-app-project/src/main/java/com/sujata/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.sujata.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Employee {
+
+	@Id
+	private int empId;
+	private String empName;
+	@Column(name = "designation")
+	private String empDesignation;
+	@Column(name ="department")
+	private String empDepartment;
+	@Column(name = "salary")
+	private Double empSalary;
+	
+	
+}
