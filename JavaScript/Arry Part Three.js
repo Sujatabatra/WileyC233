c98300fbb27e02a4d42919ce9bb5526255b124c8
Sujatabatra <sujatabatra@hotmail.com>
2022-05-24 12:59:52 +0530

    Add files via upload

---
+++ JavaScript/Arry Part Three.js
@@ -0,0 +1,17 @@
+var arr=[];
+console.log(arr);
+console.log(typeof arr);
+
+arr.push(10); //inserting at end
+arr.push(20);
+console.log(arr);
+
+arr.unshift(30);
+arr.unshift(90);
+console.log(arr);  //inserting at the begning
+
+console.log(arr.pop()); //delete at end
+console.log(arr);
+
+console.log(arr.shift());//delete at begning
+console.log(arr);
